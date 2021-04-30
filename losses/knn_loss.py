154f515e42991ac4aeddc7250e04b40fb50790b4
Eitan <eitan.kosman@gmail.com>
2021-04-30 15:20:34 +0300

    ADD cpc

--- losses/knn_loss.py
+++ losses/knn_loss.py
@@ -33,7 +33,7 @@ class KNNLoss(nn.Module):
             if self.iteration % 5000 == 0:
                 plt.figure()
                 plt.title(f"Iteration {self.iteration}, max distance: {diff.max()}")
-                plt.hist(diff.detach().cpu().numpy(), bins=1000)
+                plt.hist(diff.detach().cpu().numpy(), bins=10000)
                 plt.yscale('log')
                 # file_name = path.join(folder, f"{i}.png")
                 plt.show()
